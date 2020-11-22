2a27a6b95cc7d57999800f3c925fc82e08d46e04
philipsenwout <philipsenwout@outlook.com>
2020-11-22 17:55:54 +0100

    02. setup constructor in TokenFarm.sol add DappToken & DaiToken as paramters to constructor

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.5.0;
 
+import "./DappToken.sol";
+import "./DaiToken.sol";
+
 contract TokenFarm {
-    // All cods goes here
     string public name = 'Digital World DeFi Bank';
+    DappToken public dappToken;
+    DaiToken public daiToken;
+
+    constructor(DappToken _dappToken, DaiToken _daiToken) public {
+        dappToken = _dappToken;
+        daiToken = _daiToken;
+    }
 }
