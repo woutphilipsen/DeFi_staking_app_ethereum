f51565139c7c2d2cb076b9c6ef37b28568eebe96
philipsenwout <philipsenwout@outlook.com>
2020-11-22 18:15:34 +0100

    03. setup migration for DaiToken, DappToken & load the balanc e of the tokenfarm with 1 million & give an investor a balance

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,25 @@
+const DappToken = artifacts.require('DappToken');
+const DaiToken = artifacts.require('DaiToken');
 const TokenFarm = artifacts.require('TokenFarm');
 
-module.exports = function(deployer) {
-  deployer.deploy(TokenFarm);
+module.exports = async function(deployer, network, accounts) {
+  
+  // Deploy Mock Dai Token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
+  // Deploy Mock Dapp Token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployed()
+
+  // Deploy TokenFarm & provide addresses
+  await deployer.deploy(TokenFarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  // Transfer all tokens to tokenFarm address (1 million in total)
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
+
+  // Transfer 100 Mock DAI tokens to an investor
+  await daiToken.transfer(accounts[1], '1000000000000000000000000')
+
 }
